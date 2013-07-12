12968aae6cd765cc14cf96c4a12369bcfb62bcc3
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-12 18:53:45 +0400

    Fix $SHELL so rbenv runs the appropriate scripts

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -94,7 +94,8 @@ function npm_latest() {
 PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
 PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
 
-if [ "$(type rbenv)" ] && [ ! "$(type _rbenv)" ]; then
+# Removed && [ ! "$(type _rbenv)" ] not sure what it does right now, but it doesnt work on zsh
+if [ "$(type rbenv)" ]; then
   eval "$(rbenv init -)"
 fi
 
