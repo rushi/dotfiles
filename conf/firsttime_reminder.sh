78e8d819b2a9107994513aa76ac52826095a60ff
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-12 18:33:03 +0400

    Remove shopt and add another reminder

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -1,9 +1,9 @@
 # This file is sourced at the end of a first-time dotfiles install.
-shopt -s expand_aliases
 source ~/.zshrc
 
 cat <<EOF
 SSH Keys (if this is a server)
  1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
  2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ 3. Make sure to copy your old authorized_keys file back!
 EOF
