dd97586ab66bcfcf932c4721173d4dce83c24c86
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-12 20:36:15 +0400

    Flip boolean condition

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     echo "Looks like you're on OS X, here are a few notes:"
     echo "* You need to be an administrator (for sudo)."
     echo "* You need to have installed XCode Command Line Tools (https://developer.apple.com/downloads/index.action?=command%20line%20tools), which are available as a separate, optional (and _much smaller_) download from XCode"
-    read -n 1 -p 'Press Y to go install '
+    read -n 1 -s -p 'Press Y to go install '
     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         echo "Beginning install..."
         bash -c "$(curl -fsSL https://raw.github.com/rushi/dotfiles/master/bin/dotfiles)" && source ~/.zshrc
@@ -19,7 +19,7 @@ elif [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
     echo "Looks like you're on Ubuntu, here are a few notes:"
     echo "* You need to be an administrator (for sudo)."
     echo "* If APT hasn't been updated or upgraded recently, it will probably be a few minutes before you see anything."
-    read -n 1 -p 'Press Y to go install '
+    read -n 1 -s -p 'Press Y to go install '
     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         echo "Beginning install..."
         sudo apt-get -qq update && sudo apt-get -qq upgrade && sudo apt-get -qq install curl && echo && bash -c "$(curl -fsSL https://raw.github.com/rushi/dotfiles/master/bin/dotfiles)" && source ~/.zshrc
