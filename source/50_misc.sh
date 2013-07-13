50a69fb21f2686d7fb1c11b3c58879c4372f198b
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-13 21:09:48 +0400

    complete not required for zsh

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -8,9 +8,4 @@ function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
 }
 
-# SSH auto-completion based on entries in known_hosts.
-if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp stfp
-fi
-
 alias ps\?="ps -ef | grep -i"
