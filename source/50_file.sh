1efaad6398d35b995b3267e201a42453075e76cb
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-17 12:16:15 +0400

    Add hg-prompt

--- source/50_file.sh
+++ source/50_file.sh
@@ -48,6 +48,6 @@ export personal="$HOME/Sites/personal"
 export work="$HOME/Sites/work"
 
 # Fast directory switching
-_Z_NO_PROMPT_COMMAND=1
+#_Z_NO_PROMPT_COMMAND=1
 _Z_DATA=~/.dotfiles/caches/.z
 . ~/.dotfiles/libs/z/z.sh
--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -15,6 +15,18 @@ function prompt_char {
     echo '○'
 }
 
+hg_ps1() {
+    hg prompt "{ on {branch}}{ at {bookmark}}{status}" 2> /dev/null
+}
+
+function hg_prompt_info {
+  hg prompt --angle-brackets "\
+< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
+%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
+patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
+}
+
 PROMPT='
-%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(git_prompt_info)
+%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(git_prompt_info)$(hg_prompt_info)
 $(prompt_char) '
