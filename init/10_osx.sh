f72b9157517877f5e926eeebdd4149f0887f9f7d
Rushi Vishavadia <rushi.v@gmail.com>
2014-08-28 20:16:40 +0530

    Add htop

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -19,7 +19,7 @@ if [[ "$(type -P brew)" ]]; then
   brew update
 
   # Install Homebrew recipes.
-  recipes=(git mercurial tree sl lesspipe nmap git-extras htop-osx man2html zsh)
+  recipes=(git mercurial tree sl lesspipe nmap git-extras htop-osx man2html zsh htop)
 
   list="$(to_install "${recipes[*]}" "$(brew list)")"
   if [[ "$list" ]]; then
