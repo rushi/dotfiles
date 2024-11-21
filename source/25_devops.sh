7fbb52c79fa4572c9bd2e64ed2602c0280d8d9d9
Rushi Vishavadia <rushi.v@gmail.com>
2024-11-21 14:56:41 +0530

    Cleanup aws methods

--- source/25_devops.sh
+++ source/25_devops.sh
@@ -4,68 +4,6 @@ function slsdeploy() {
     set +x
 }
 
-# TODO: ec2 start <instance-id>
-# TODO: ec2 start router-01
-# TODO: ec2 start router-01.xola.com
-# TODO: Show, Stop, Restart
-
-# {
-#   "Changes": [
-#     {
-#       "Action": "UPSERT",
-#       "ResourceRecordSet": {
-#         "Name": "<DomainName>",
-#         "Type": "A",
-#         "TTL": 300,
-#         "ResourceRecords": [
-#           {
-#             "Value": "<NewIP>"
-#           }
-#         ]
-#       }
-#     }
-#   ]
-# }
-
-function foo() {
-    ZONE_NAME=$1
-    RECORD_NAME=$2
-    RECORD_TYPE=$3
-    IP_ADDRESS=$4
-
-    ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[?Name == '${ZONE_NAME}.'].Id" --output text)
-
-    if [ -z "$ZONE_ID" ]; then
-        echo "Zone ID not found for ${ZONE_NAME}"
-        exit 1
-    fi
-
-    CHANGE_BATCH=$(
-        cat <<EOF
-{
-  "Changes": [
-    {
-      "Action": "UPSERT",
-      "ResourceRecordSet": {
-        "Name": "${RECORD_NAME}",
-        "Type": "${RECORD_TYPE}",
-        "TTL": 300,
-        "ResourceRecords": [
-          {
-            "Value": "${IP_ADDRESS}"
-          }
-        ]
-      }
-    }
-  ]
-}
-EOF
-    )
-
-    aws route53 change-resource-record-sets --hosted-zone-id "${ZONE_ID}" --change-batch "${CHANGE_BATCH}"
-
-}
-
 function ec2_id() {
     # aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PublicIpAddress:PublicIpAddress,State:State.Name,Name:Tags[?Key=='Name']}"
     aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query "Reservations[*].Instances[*].{Name:Tags[?Key=='Name'],InstanceId:InstanceId,PublicIpAddress:PublicIpAddress,State:State.Name}" | jq '.'
