c3cf3e0a672a363396c85ec596e0db28496a01e1
Rushi Vishavadia <rushi.v@gmail.com>
2025-11-05 07:59:08 +0530

    Extra packages

--- source/initNodeProject.mjs
+++ source/initNodeProject.mjs
@@ -1,7 +1,10 @@
 #!/usr/bin/env zx --quiet
 
-const dependencies = ["axios", "lodash", "picocolors", "config", "dayjs", "prettyoutput", "toml"];
-const devDependencies = ["@types/node", "@types/config", "@types/lodash", "prettier"];
+const dependencies = ["axios", "lodash-es", "chalk", "config", "dayjs", "prettyoutput", "js-yaml"];
+const devDependencies = ["@types/node", "@types/config", "@types/lodash-es", "prettier"];
+const extraDependencies = ["vite", "tailwindcss @tailwindcss/vite @vitejs/plugin-react tailwind-merge clsx"]; // Multiple in a single string means all together
+const extraDevDependencies = ["prettier-plugin-tailwindcss"];
+
 const cmd = (c) => c?.toString().trim() ?? null;
 
 const prettier = {
@@ -14,6 +17,7 @@ const prettier = {
     arrowParens: "always",
     bracketSameLine: false,
     tabWidth: 4,
+    plugins: [],
 };
 
 const editorConfig = `
@@ -39,9 +43,26 @@ indent_size = 2
 `;
 
 const initNpmProject = async () => {
-    if (fs.existsSync("package.json") || fs.existsSync("node_modules")) {
-        console.log(chalk.red("Warning package.json or node_modules already exists in this dir\n"));
+    if (fs.existsSync("package.json") || fs.existsSync("package-lock.json") || fs.existsSync("node_modules")) {
+        // Read the name of the existing app and the existing directory
+        const existingName = fs.existsSync("package.json") ? (await fs.readJson("./package.json")).name : null;
+        console.log(chalk.red(`Warning this folder has existing data. App: '${existingName}'`));
+        const proceed = await question(`Should I proceed? [y/n/remove] `);
+        if (proceed.includes("remove")) {
+            console.log(chalk.yellow("Removing existing package.json, package-lock.json, and node_modules..."));
+            await $`rm -rf package.json package-lock.json node_modules`;
+        } else if (!proceed.includes("y")) {
+            console.log(chalk.red("Exiting without changes."));
+            process.exit(0);
+        } else {
+            console.log(chalk.yellow("Proceeding with existing files, but this may cause issues."));
+        }
+        console.log();
     }
+
+    console.log(chalk.blue("Initializing a new Node.js project with Node v20"));
+    await fs.writeFileSync(".nvmrc", "v20");
+    await $`fnm use`;
 
     const nodeVersion = cmd(await $`node -v`).slice(0, 3);
     console.log(`Node: ${chalk.green(nodeVersion)}`);
@@ -53,30 +74,58 @@ const initNpmProject = async () => {
         devDependencies.push("typescript", "type-fest");
     }
 
-    await $`npm init -y`;
+    await $`npm init -y`
+    await $`npm pkg set type=module`;
 
-    console.log(`Installing dependencies ${dependencies.join(", ")}`);
+    console.log(`Installing dependencies ${dependencies.map(p => chalk.bold(p)).join(", ")}`);
     await $`npm install ${dependencies}`;
 
-    console.log(`Installing devDependencies ${devDependencies.join(", ")}`);
+    console.log(`Installing devDependencies ${devDependencies.map(p => chalk.bold(p)).join(", ")}`);
     await $`npm install --save-dev ${devDependencies}`;
 
     const main = isTypescript ? "app.ts" : "app.js";
-    const pkg = {
-        ...(await fs.readJson("./package.json")),
+    await $`touch ${main}`;
+    console.log(`Created main file: ${chalk.bold(main)}`);
+
+    let pkg = { ...(await fs.readJson("./package.json")), prettier };
+    pkg = await handleExtraPackages(pkg, extraDependencies, false);
+    pkg = await handleExtraPackages(pkg, extraDevDependencies, true);
+    pkg = {
+        ...(await fs.readJson("./package.json")), // This will contain the new dependencies installed above
         name,
+        description: "TBD",
         main,
-        scripts: {},
+        prettier,
         private: true,
         engines: { node: `>=${nodeVersion.replace("v", "")}` },
-        prettier,
-    };
+    }
 
     await fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2));
-    console.log(chalk.green("\nAll done, here is your package.json"));
+    console.log("\n✅ All done, here is your package.json");
     echo(await $`npx prettyoutput package.json`);
 
     await fs.writeFileSync(".editorconfig", editorConfig);
+};
+
+const handleExtraPackages = async (pkg, extras, isDev = false) => {
+    console.log(`\nThere are some extra ${isDev ? "devDependencies" : "dependencies"} you can install`, chalk.bold(extras));
+    for (const extra of extras) {
+        const install = await question(`Install ${chalk.green(extra + "?")} [y/n] `);
+        if (install.includes("y")) {
+            console.log(` Installing ${chalk.bold(extra)}...`);
+            await $`npm install ${isDev ? "--save-dev" : "--save"} ${extra.split(" ")}`;
+            if (extra.includes("vite") && !isDev) {
+                pkg.scripts.start = "vite";
+                pkg.scripts.build = "vite build";
+                pkg.scripts.dev = "vite";
+            }
+            if (extra.includes("prettier-plugin-tailwindcss")) {
+                pkg.prettier.plugins = ["prettier-plugin-tailwindcss"];
+            }
+        }
+    }
+
+    return pkg;
 };
 
 initNpmProject();
